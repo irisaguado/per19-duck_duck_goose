7ea6d2a16ab2def0712f3d517f35449f8fd60893
iris aguado <i.aguado.2018@alumnos.urjc.es>
2019-01-21 16:05:34 +0100

    Mi commit

---
+++ duck_duck_goose1.py
@@ -0,0 +1,11 @@
+import sys
+nom_jugadores = ['juan', 'pepa', 'antonio']
+num_elegigo = sys.argv[1]
+num_elegido = int(num_elegido)
+def duck_duck_goose(nom_jugadores, num_elegido):
+    while num_elegido > len(nom_jugadores):
+        num_elegido = num_elegido -4
+    jug_elegido = nom_jugadores[num_elegido-1]
+    return jug_elegido
+
+print(duck_duck_goose(nom_jugadores, num_elegido))
